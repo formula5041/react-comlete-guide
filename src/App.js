67ca891518d3fc8005a4244de77b801f8a9143fa
RedTsai <red@RedTsaideMacBook-Air.local>
2022-09-17 10:30:05 +0800

    [Lesson 60 done]

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ const App = () => {
       date: new Date(2022, 5, 12),
     },
   ]
+
+  const addExpenseHnadler = (expenseData) =>{
+    console.log('On App.js')
+    console.log(expenseData)
+  }
+  
+
+
   // return React.createElement (
   //   'div',
   //   {className:"App"},
@@ -36,7 +44,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <NewExpense/>
+        <NewExpense onAddExpense = {addExpenseHnadler}/>
         <Expenses
           items={expensesData}
         />
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,21 @@ import React from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
-const NewExprense = () => {
-  return <div className='new-expense'>
-    <ExpenseForm />
-  </div>
+const NewExprense = (props) => {
+  const saveExpenseDataHandler = (enderedExpenseData)=>{
+    const expenseData = {
+      ...enderedExpenseData,
+      id: Math.random().toString(),
+    }
+    props.onAddExpense(expenseData)
+
+  }
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
+    </div>
+  )
 }
 
 export default NewExprense
